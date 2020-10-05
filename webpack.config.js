9cceefd6b56384e2d8f627fa6d5d546d3dbc7001
vigeb <reach2101@gmail.com>
2020-10-05 21:16:49 +0700

    update css and html as well as webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: './app',
+        hot: true,
+        port: 5000,
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader' ,'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: postCSSPlugins
+                        }
+                    }
+                }]
+            }
+        ]
+    }
+}
